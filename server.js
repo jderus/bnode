61d6f3bed49a13a269f8af5ae56abed16f7c6bd0
jderus <jderus@gmail.com>
2016-02-15 13:28:36 -0800

    Bootstrap app and add a server module.

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var http = require("http");
 
-function onRequest(request,response) {
-  //Note: might see 2 requests on browser load due to favicon request.
-  console.log("Request received");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello Node Server");
-  response.end();
+function start() {
+  function onRequest(request,response) {
+    //Note: might see 2 requests on browser load due to favicon request.
+    console.log("Request received");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello Node Server");
+    response.end();
+  }
+  http.createServer(onRequest).listen(8888);
+  console.log("server has started...");
 }
 
-http.createServer(onRequest).listen(8888);
-console.log("server has started...");
+exports.start = start;
