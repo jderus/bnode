a107081ba1899e6679773bbae2d97d40c26b1d28
jderus <jderus@gmail.com>
2016-02-15 13:38:10 -0800

    Add routing module

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var http = require("http");
+var url = require("url");
 
-function start() {
+function start(route) {
   function onRequest(request,response) {
+    var pathname = url.parse(request.url).pathname;
     //Note: might see 2 requests on browser load due to favicon request.
-    console.log("Request received");
+    console.log("Request received for :" + pathname);
+
+    route(pathname);
+    
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello Node Server");
     response.end();
