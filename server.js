0c99981ac782cc80b90356a7103b756a791ba56a
jderus <jderus@gmail.com>
2016-02-15 14:58:34 -0800

    POSTing and gitignore

--- server.js
+++ server.js
@@ -4,10 +4,22 @@ var url = require("url");
 function start(route, handle) {
   function onRequest(request,response) {
     var pathname = url.parse(request.url).pathname;
+    var postData = "";
     //Note: might see 2 requests on browser load due to favicon request.
     console.log("Request received for: " + pathname);
 
-    route(handle, pathname, response);
+    request.setEncoding("utf8");
+
+    request.addListener("data", function(postDataChunk) {
+      postData += postDataChunk;
+      console.log("Received POST data chunk '" +
+      postDataChunk + "'.");
+    })
+
+    request.addListener("end", function() {
+          route(handle, pathname, response,postData);
+    });
+
   }
   http.createServer(onRequest).listen(8888);
   console.log("server has started...");
