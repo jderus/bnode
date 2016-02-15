da5595986b90a5fece25012f82978e969eb59a93
jderus <jderus@gmail.com>
2016-02-15 13:51:14 -0800

    Add request handlers.

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var http = require("http");
 var url = require("url");
 
-function start(route) {
+function start(route, handle) {
   function onRequest(request,response) {
     var pathname = url.parse(request.url).pathname;
     //Note: might see 2 requests on browser load due to favicon request.
-    console.log("Request received for :" + pathname);
+    console.log("Request received for: " + pathname);
 
-    route(pathname);
-    
+    route(handle, pathname);
+
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello Node Server");
     response.end();
