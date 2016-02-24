30a728c6a68f1386c70e0db17b48c7112e0ce983
jderus <jderus@gmail.com>
2016-02-23 21:21:26 -0800

    pipe out child proc

--- http_server.js
+++ http_server.js
@@ -10,11 +10,27 @@
 //    res.end(util.inspect(req.headers)); 
 // }).listen(4000);
 
-var fs = require('fs');
-var util = require('util');
+// Note: this example doesnt seem to work in chrome.
+// var fs = require('fs');
+// var util = require('util');
+// 
+// require('http').createServer(function(req,res){
+//    res.writeHead(200, {'Content-Type': 'video/mp4'});
+//    var rs = fs.createReadStream('test.mp4');
+//    rs.pipe(res);
+// }).listen(4000);
 
-require('http').createServer(function(req,res){
-   res.writeHead(200, {'Content-Type': 'video/mp4'});
-   var rs = fs.createReadStream('test.mp4');
-   rs.pipe(res);
+// Pipe out from a child proc
+var spawn = require('child_process').spawn;
+ 
+ require('http').createServer(function(req,res){
+   var child = spawn('cmd.exe', 
+     ['dir'],
+     {stdio:'inherit'});
+     if(child.stdout !== null){
+        child.stdout.pipe(res);
+        res.on('end', function() {
+            child.kill(); 
+        });
+     }
 }).listen(4000);
