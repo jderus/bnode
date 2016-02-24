112e6a77d2f81f2cfd48ca9ad426568bb4cf4f8b
jderus <jderus@gmail.com>
2016-02-23 21:11:59 -0800

    Stream an mp4. No mp4 checkin.

--- http_server.js
+++ http_server.js
@@ -4,8 +4,17 @@
 //    res.end('Hello Node'); 
 // }).listen(4000);
 
+// var util = require('util');
+// require('http').createServer(function(req,res){
+//    res.writeHead(200, {'Content-Type': 'text/plain'});
+//    res.end(util.inspect(req.headers)); 
+// }).listen(4000);
+
+var fs = require('fs');
 var util = require('util');
+
 require('http').createServer(function(req,res){
-   res.writeHead(200, {'Content-Type': 'text/plain'});
-   res.end(util.inspect(req.headers)); 
+   res.writeHead(200, {'Content-Type': 'video/mp4'});
+   var rs = fs.createReadStream('test.mp4');
+   rs.pipe(res);
 }).listen(4000);
