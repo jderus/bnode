688e978031cc5625ecf5b09b4b02d964b29de2c0
jderus <jderus@gmail.com>
2016-02-28 17:09:05 -0800

    http redirect

--- http_request.js
+++ http_request.js
@@ -1,8 +1,25 @@
 //this doesnt appear to keep-alive
+// var request = require('request');
+// var inspect = require('util').inspect;
+// 
+// request('http://localhost:4400/abc/def',
+//     function(err,res,body){
+//         if(err) { throw err; }
+//         console.log(inspect({
+//             err: err,
+//             res: {
+//                 statusCode: res.statusCode
+//             },
+//             body: JSON.parse(body)
+//         })) 
+//     }
+// );
+
 var request = require('request');
 var inspect = require('util').inspect;
 
-request('http://localhost:4400/abc/def',
+request(
+    'http://localhost:4400/redirect',
     function(err,res,body){
         if(err) { throw err; }
         console.log(inspect({
